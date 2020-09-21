1a0be8628b64ce60ec13fb0341247bb6485f5e8e
luccalipisky <luccalipisky@gmail.com>
2020-09-20 22:06:22 -0300

    Updates

---
+++ precache-manifest.624e309613e14251413b419915e1f304.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19d00ed0f53e73eba875e1fefc51824",
+    "url": "/PomodoroTimer/index.html"
+  },
+  {
+    "revision": "d0ff19ef2009817e5491",
+    "url": "/PomodoroTimer/static/css/main.ae5791ea.chunk.css"
+  },
+  {
+    "revision": "baebf68398b198a27881",
+    "url": "/PomodoroTimer/static/js/2.f72d3ebf.chunk.js"
+  },
+  {
+    "revision": "0c642dbff179889abda1a42753a27d19",
+    "url": "/PomodoroTimer/static/js/2.f72d3ebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0ff19ef2009817e5491",
+    "url": "/PomodoroTimer/static/js/main.112b91c8.chunk.js"
+  },
+  {
+    "revision": "bff7b314cba3548e6aae",
+    "url": "/PomodoroTimer/static/js/runtime-main.bbcbb8b2.js"
+  },
+  {
+    "revision": "7294772be10c99791abf35c5c4d09758",
+    "url": "/PomodoroTimer/static/media/play.7294772b.svg"
+  },
+  {
+    "revision": "ea2646575bad4d41647ec226b4600995",
+    "url": "/PomodoroTimer/static/media/profile.ea264657.jpeg"
+  },
+  {
+    "revision": "7b1b3b1f9d0d7596ac24b9e087286b4b",
+    "url": "/PomodoroTimer/static/media/reset.7b1b3b1f.svg"
+  },
+  {
+    "revision": "fe1072bedf153b53929b049e41fcc0f6",
+    "url": "/PomodoroTimer/static/media/stop.fe1072be.svg"
+  },
+  {
+    "revision": "d8d435aac6f59bf8d80c051195fdc26f",
+    "url": "/PomodoroTimer/static/media/swiftly.d8d435aa.mp3"
+  }
+]);
